8574baae9c2e0adba99a744793f125f7be746bc9
Negar Lashgari <negar.lashgari@gmail.com>
2023-04-18 13:20:12 -0400

    initial commit

---
+++ Lesson1/main.js
@@ -0,0 +1,4 @@
+var username = "Negar";
+console.log(username);
+//working with 1 file, command is tsc main.ts -w
+//which is tsc file name -w (watch mode)
---
+++ Lesson1/main.ts
@@ -0,0 +1,5 @@
+let username = "Negar";
+console.log(username);
+
+//working with 1 file, command is tsc main.ts -w
+//which is tsc file name -w (watch mode)
